d62fe854b2079e4b9f42bc2ab5379eab4a1d0c41
Andhika Haqqa Syahrony <bban97550@gmail.com>
2025-11-12 19:42:29 +0700

    orderRoutes.js change

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,11 +1,14 @@
+// routes/orderRoutes.js
 const express = require('express');
 const router = express.Router();
-const purchaseController = require('../controllers/orderController');
-const {authReq} = require('../middleware/authMiddleWare');
+const orderController = require('../controllers/orderController');
+const { authReq } = require('/middleware/authMiddleWare');
 
-router.post('/cart', authReq, purchaseController.addToCart);
-router.post('/order', authReq, purchaseController.addOrder);
-router.post('/payment', authReq, purchaseController.addPayment);
+// Semua route order memerlukan login
+router.use(authReq);
+
+router.post('/', orderController.addOrder); // Checkout
+router.get('/:id', orderController.getOrderById);
+router.post('/:id/payments', orderController.addPayment); // <-- URL Diubah
 
 module.exports = router;
-
